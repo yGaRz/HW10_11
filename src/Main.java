4879ac6a082013eab62f31113a26ac72ce316dcb
yGaR <ygar@rambler.ru>
2022-03-17 20:51:15 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,7 @@ public class Main {
                 inp = scanner.nextInt();
                 switch (inp){
                     case 1:{
-
+                        createFile();
                         break;
                     }
                     case 2:{
@@ -35,12 +35,50 @@ public class Main {
             catch (Exception ex){
                 System.out.println(ex.getMessage());
             }
+        }
+    }
+    public static void createFile(){
+        System.out.println("Введите имя файла с расширением:");
+        Scanner scanner = new Scanner(System.in);
+        String strName = scanner.nextLine();
+        int inp =0;
+        while (true) {
+            try {
+                inp = scanner.nextInt();
+                switch (inp) {
+                    case 1: {
 
+                        break;
+                    }
+                    case 2: {
+
+                        break;
+                    }
+                    case 3: {
+
+                        break;
+                    }
+                    case 0: {
+
+                        return;
+                    }
+                    default: {
+                        System.out.println("Выбран неверный пункт меню.");
+                    }
+                }
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
         }
     }
 
-
-
+    public static void printCreateFileMenu(){
+        System.out.println("Выберите тип:");
+        System.out.println("1. JSON.");
+        System.out.println("2. XML.");
+        System.out.println("3. Текстовый.");
+        System.out.println("0. Возврат.");
+    }
 
     public static void printMainMenu(){
         System.out.println("Введите пункт меню:");
